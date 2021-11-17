e587371cdeec1b57730a9f8ae4aa3297d41e20c2
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-11-17 20:28:24 +0530

    Add files via upload

---
+++ PasswordChecking/PasswordChecking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace PasswordChecking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email;
+            Console.WriteLine("Please Enter your E-mail Id :");
+            email = Console.ReadLine();
+            Regex regex = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0 - 9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            RegexOptions.CultureInvariant | RegexOptions.Singleline);
+            Console.WriteLine($"The email is {email}");
+            bool isValidEmail = regex.IsMatch(email);
+            if (!isValidEmail)
+            {
+                Console.WriteLine($"The email is invalid");
+
+            }
+            else
+            {
+                Console.WriteLine($"The email is valid");
+            }
+
+            Console.Read();
+        }
+    }
+}
+        
+    
